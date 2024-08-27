5955f69d34267c8f14e6623ef1d5e2b20f1b06f0
BasilKhan07 <basilkhan06@gmail.com>
2024-08-27 22:53:31 +0500

    Relational and Arithmetic Operators

---
+++ 04-Operators/Operators/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = 4;
+            var b = 2;
+
+            Console.WriteLine(a + b);
+            Console.WriteLine(a - b);
+            Console.WriteLine(a * b);
+            Console.WriteLine(a / b);
+            Console.WriteLine(a % b);
+
+            Console.WriteLine(a > b);
+            Console.WriteLine(a >= b);  
+            Console.WriteLine(a <= b);
+            Console.WriteLine(a == b);
+            Console.WriteLine(a != b);
+        }
+    }
+}
